dadbf98d26012d777c419ee78b46c02a0da3636b
Mohd Amaan <mhdamaan79@gmail.com>
2023-08-10 17:04:18 +0530

    enhanced styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import NavBar from "./components/NavBar";
 import AddToDo from "./components/AddToDo";
 import ToDos from "./components/ToDos";
 import "./App.css";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
     <main>
-      <h1>REACTOTYPE TODO</h1>
+      <div className="header">
+        <img className="logo" src={logo} alt="logo" />
+        <h2>REACTOTYPE TO-DO</h2>
+      </div>
       <NavBar />
       <AddToDo />
       <ToDos />
